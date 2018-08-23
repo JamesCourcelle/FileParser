c1255a292cf6924621bffccd7eefa36aae6c920e
james.courcelle1@gmail.com <james.courcelle1@gmail.com>
2018-08-22 22:29:48 -0400

    Created Node class
    Created Node() Class and ensured that bid can be loaded into the  a node.

--- search_file.py
+++ search_file.py
@@ -10,10 +10,19 @@ class Bid():
 
     def load_bid(self, bid):
         self.bid_id = int(bid["Auction ID"])
-        self.title = bid["Auction Title "]
+        self.title = bid["Auction Title"]
         self.fund = bid["Fund"]
         self.amount = bid["Auction Fee Total"]
-		
+        
+        
+class Node():
+    def __init__(self):
+        self.right = None
+        self.left = None
+        
+    def set_bid(self, a_bid):
+        self.bid = a_bid
+        
 		
 list_bids = parse_csv.open_file('eBid_Monthly_Sales.csv')
 
@@ -35,6 +44,7 @@ def contains_bid(bid_list, check):
         if check(bid):
             counter += 1
             print("The count is %s" % counter)
+            print("The bid ID is %s and the count is %s" % (bid.bid_id, counter))
             return True
         counter += 1
         print("The bid ID is %s and the count is %s" % (bid.bid_id, counter))
@@ -45,6 +55,18 @@ if contains_bid(new_list, lambda bid: bid.bid_id == check_bid):
     print("Bid was found")
 else:
     print("Bid was not found")
+    
+print()
+
+print("Testing the Node class and function")
+test_node = Node()
+test_node.set_bid(new_list[17936])
+
+# Testing the node class and components
+print("The bid in the nod is %s" % test_node.bid.bid_id)
+print("The node's left is %s" % test_node.left)
+print("The node's right is %s" % test_node.right) 
+
         
 	
 
